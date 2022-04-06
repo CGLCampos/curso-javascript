0e63c2f56d378089a02a52d72a118e12afdc1c19
cosme.campos <cglcampos96@gmail.com>
2022-04-06 09:21:46 -0300

    Adicionado formulário para inserção de pacientes na tabela

--- js/principal.js
+++ js/principal.js
@@ -31,8 +31,53 @@ for(var i = 0; i < pacientes.length; i++){
     } 
     
     if (pesoValido && alturaValida) {
-        var imc = peso / (altura * altura);
-        tdImc.textContent = imc.toFixed(2);
+        tdImc.textContent = calcularImc(peso, altura);
     }
 }
 
+function calcularImc (peso, altura) {
+    var imc = peso / (altura * altura);
+    return imc.toFixed(2);
+}
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event){
+    event.preventDefault();
+
+    var form = document.querySelector("#form-adiciona");
+    
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("tr");
+    pacienteTr.classList.add("paciente");
+
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.classList.add("info-nome");
+    pesoTd.classList.add("info-peso");
+    alturaTd.classList.add("info-altura");
+    gorduraTd.classList.add("info-gordura");
+    imcTd.classList.add("info-imc");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+    imcTd.textContent = calcularImc(peso, altura);
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
+
+    var table = document.querySelector("#tabela-pacientes");
+    table.appendChild(pacienteTr);
+});
